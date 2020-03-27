734800522e324efc8488415f8bc66c89500fbf35
viX-shaw <songokurocks551@gmail.com>
2020-03-27 20:28:31 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -11,22 +11,29 @@ export default class App extends Component {
   state = {
     uploading: false,
     image: null,
+    idx: null,
     sampleImageURLs: ['https://drive.google.com/uc?id=1IyVHYIGVz7ENggzYh3tn6OrbjqXzwloj',
                       'https://drive.google.com/uc?id=1VbIKLM0JT4SFO5d-sDXYNa0JbcFCm-gk',
                       'https://drive.google.com/uc?id=1e7LErL-c78sCsa3y9Rd_wpD4T4MiaiKn',
                       'https://drive.google.com/uc?id=1_BTB34MS6AAX3bjy91b6Gfvx2E08w4qn',
-                    'https://drive.google.com/uc?id=1IDjIhH5HZrnR32JT_TeUtmhib1KaWp4I']
+                    'https://drive.google.com/uc?id=1IDjIhH5HZrnR32JT_TeUtmhib1KaWp4I',
+                    'https://drive.google.com/uc?id=1JHSOV-3cDjyl5c2c_QmX7ZhZYkS0wKi0',
+                    'https://drive.google.com/uc?id=1DBB0m96_5CN3cjEBRK4nS137jj85XMvI',
+                    'https://drive.google.com/uc?id=1GZMRR0wts4a6Y7BZ0THVZDEUlUodrgeU',
+                    'https://drive.google.com/uc?id=1t9P7DCu3_PvWLDlljisN64yOGEf_dFkM',
+                    'https://drive.google.com/uc?id=1YS_Vu19SinBZeF5MO9j0NtLkXkD7dafm']
+
   }
 
-  fetchdata = (formdata)=> {
-    this.setState({ uploading: true })
+  fetchdata = (formdata, idx)=> {
+    this.setState({ uploading: true, idx: idx })
     console.log(typeof(formdata) == "string")
     console.log(formdata)
     if (typeof(formdata) == "string"){
       formdata = JSON.stringify({"url": formdata})
     }
 
-    const myRequest = new Request('http://localhost:5000/classify', {
+    const myRequest = new Request('/classify', {
       method: 'POST',
       body: formdata,
       // cache: 'default',
@@ -75,30 +82,42 @@ export default class App extends Component {
 
     return (
       <div>
-        <div style={{ flex: 1, padding: 5, justifyContent: "center" ,boxShadow: "0px 3px 10px 1px #9E9E9E"}}>
+        <div style={{ flex: 1, padding: 5, justifyContent: "center" }}>
           <h1>CrowdEye</h1>
         </div>
         {/* <div className="container"> */}
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ flex: "50%", padding: 10}}>
-            <h2>Test on Sample Images</h2>
-              {
-                this.state.sampleImageURLs.map(imgpath =>{
-                  return <img src={imgpath} alt='' onClick={()=>this.fetchdata(imgpath)} style={{padding: 5, cursor:"pointer", width:100, height:100}}/>
-                })
-              }
-            {/* <h3 style={{paddingLeft: 30}}>or</h3> */}
-            <h2>Upload Image</h2>
-            <div className='buttons'>
-              <Buttons onChange={this.onChange} />
-            </div>
-          </div>
-          <div style={{flex: "50%", backgroundColor: "green"}}>
-            <h2 style={{color: "white", paddingLeft: 20}}>Result</h2>
+        <div style={{flex: "50%", backgroundColor: "green"}}>
+            {
+              !this.state.image ?
+              <h2 style={{color: "white", padding: 20}}>Select an Image from the list below</h2>
+              :
+              <h2 style={{color: "white", padding: 20}}>Result</h2>
+            }
             <div style={{display: "flex", justifyContent: "center", padding: 10}}>
               {content()}
             </div>
           </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ flex: "50%", padding: 10}}>
+            <h2>Test on Sample Images</h2>
+              {
+                this.state.sampleImageURLs.map((imgpath, idx) =>{
+                  if(this.state.idx === idx){
+                    const style = {padding: 5, cursor:"pointer", width:100, border: '1px solid #021a40'} 
+                    return <img src={imgpath} alt='' onClick={()=>this.fetchdata(imgpath, idx)} style={style}/>
+                  }else{
+                    const style = {padding: 5, cursor:"pointer", width:100} 
+                    return <img src={imgpath} alt='' onClick={()=>this.fetchdata(imgpath, idx)} style={style}/>
+                  }
+                })
+              }
+            {/* <h3 style={{paddingLeft: 30}}>or</h3> */}
+            {/* <h2>Upload Image</h2>
+            <div className='buttons'>
+              <Buttons onChange={this.onChange} />
+            </div> */}
+          </div>
+          
           {/* <p style={{backgroundColor: "green"}}>asdvvnjdfv sdfdfk</p> */}
         </div>
       </div>
