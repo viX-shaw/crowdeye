4ef8b525359a02a3cf962b96ea23ad8180e0526d
viX-shaw <songokurocks551@gmail.com>
2020-03-27 11:07:32 +0530

    Updates

---
+++ precache-manifest.3679de812c8de3106d83710a568fc551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31743ae70c3be56d6862e519dcc8b09a",
+    "url": "/crowdeye/index.html"
+  },
+  {
+    "revision": "d3cd3683a4251a5a97c9",
+    "url": "/crowdeye/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dcc633eed092913a62a4",
+    "url": "/crowdeye/static/js/2.f9ab193d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/crowdeye/static/js/2.f9ab193d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cd3683a4251a5a97c9",
+    "url": "/crowdeye/static/js/main.fead6b17.chunk.js"
+  },
+  {
+    "revision": "ace2a2fa3770774179b2",
+    "url": "/crowdeye/static/js/runtime-main.bd510ddb.js"
+  }
+]);
